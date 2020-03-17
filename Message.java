162a73dff88f36331163b1424d226929f96dc86b
EndrinB <58371775+EndrinB@users.noreply.github.com>
2020-03-17 20:26:35 +0100

    Update Message.java

--- Message.java
+++ Message.java
@@ -5,6 +5,8 @@ public class Message{
    File messages = new File("messages.txt");
    Scanner scan;
    int key;
+   FileWriter writer;
+   
    Message(int key){
       this.key = key;
    }
@@ -20,11 +22,48 @@ public class Message{
             }else{
                scan.nextLine();
             }
+            scan.close();
             
          }
       }catch(Exception e){}
-      if(msg.size() == 0){System.out.println("Either no messages for the user or key is invalid");}
-      
       return msg;
+   }
+   public String addMessage(String message){
+               
+      try{
+         scan = new Scanner(messages);
+         while(scan.hasNextLine()){
+            if(key == Integer.parseInt(scan.next())){
+               writer = new FileWriter(messages,true);
+               writer.write(" " + message + " ");
+            
+               writer.flush();
+               writer.close();
+            }
+            else{
+               scan.nextLine();
+            }
+         }
+      }catch(Exception e){}
+      return message;
+   }
+   public String removeMessage(int num){
+          String message = "";     
+      try{
+         scan = new Scanner(messages);
+         while(scan.hasNextLine()){
+            if(key == Integer.parseInt(scan.next())){
+               while(num >1){
+                  if(scan.hasNextInt()){System.out.println("you dont have that many messages");}
+                  else{scan.next(); num--;}
+               }
+               message = scan.next();
+            }
+            else{
+               scan.nextLine();
+            }
+         }
+      }catch(Exception e){}
+      return message;
    }
 }
