7293e78f211c7cde48a5440992f73986d0726266
EndrinB <58371775+EndrinB@users.noreply.github.com>
2020-03-22 15:35:08 +0100

    Update MessageHandler.java

--- MessageHandler.java
+++ MessageHandler.java
@@ -4,14 +4,17 @@ import java.util.*;
 public class MessageHandler{
    File messages = new File("messages.txt");
    private LinkedList<Integer> userKeys = new LinkedList();
-  
+   private HashMap<Integer,String> allMessages = new HashMap<Integer,String>();
    MessageHandler(){
    try{
     Scanner scan = new Scanner(messages);
-    scan.useDelimiter("[, \n]");
+    scan.useDelimiter("[,\n]");
     while(scan.hasNextLine()){
-      userKeys.add(Integer.parseInt(scan.next()));
-      scan.nextLine();
+      String s = scan.nextLine();
+      int uKey = Integer.parseInt(s.charAt(0) +"");
+      String allMsg = s.substring(2);
+      userKeys.add(uKey);
+      allMessages.put(uKey,allMsg);
     }
     scan.close();
     
@@ -19,6 +22,24 @@ public class MessageHandler{
    }
    
    public boolean containsMsgKey(int k){
-      return (userKeys.get(k-1) == k);
+      return (userKeys.contains(k));
+   }
+   public void userRemoved(Integer[] array,int key){
+               userKeys.remove(new Integer(key));
+               allMessages.remove(key);
+               int count = 0;
+               try{
+            FileWriter wr = new FileWriter(messages,false);
+            BufferedWriter bw = new BufferedWriter(wr);
+            PrintWriter pw = new PrintWriter(bw);
+            while(count <= array.length-1){
+               wr.write(0+array[count] +"," + allMessages.get(array[count]) +"\n" );
+               count++;
+            }
+            wr.flush();
+            wr.close();
+         
+         
+         }catch(Exception e){}
    }
 }
